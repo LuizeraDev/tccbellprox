a17f7d1781a856e21b80fd01729a4b32931e0792
Paulo <henriquerosendo10@gmail.com>
2019-11-20 23:52:27 -0200

    alterado profissional

--- profissional/index.php
+++ profissional/index.php
@@ -1,3 +1,22 @@
-<?php
-echo "olá voce eeta na pagina do profissional";
+<?php 
+include "../login/confirma-login-paginas.php";
 ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">
+<title>Bell Prox - Meu Perfil</title>
+<!--LOGO-->
+<link rel="shortcut icon" href="../img/logo.png" />
+<!--MAIN CSS-->
+<link rel="stylesheet" type="text/css" href="../index/main.css" media="screen" />
+<link rel="stylesheet" type="text/css" href="../css/profEstilo.css" />
+<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" media="screen" />
+</head>
+<body>
+<?php 
+    include "../index/menu.php";
+    include "conteudo-profissional.php";
+?>
+</body>
+</html>
