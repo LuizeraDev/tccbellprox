b7ef2046a1c83a7fb36a4f560a66b252094dc56a
Luiz Guilherme <geekanimesamvs@gmail.com>
2019-12-03 16:21:08 -0300

    mudancas

--- paginas/realizarAgendamento.php
+++ paginas/realizarAgendamento.php
@@ -22,9 +22,49 @@
     $cd_cliente = $informacoes[0]['cd_cliente'];
     $cd_profissional = $_GET['id'];
 
+
     $contador = 0;
-    
+
+
+    date_default_timezone_set('America/Sao_Paulo');
+    $dataatual = date('d/m/y');
+    $horaatual = date('H:i:s');
+
+    $horaatualizada = $horaatual[0].$horaatual[1];
+    $atual = (int)$horaatualizada-1;
+    $hora = $tempo[0].$tempo[1];
+
+
+    $diaatual = $dataatual[0].$dataatual[1];  
+    $mesatual = $dataatual[3].$dataatual[4]; 
+    $anoatual = $dataatual[6].$dataatual[7]; 
+
     $ano = $data[0].$data[1].$data[2].$data[3];
+    $mes = $data[5].$data[6];
+    $dia =  $data[8].$data[9]; 
+    
+    if($dia < $diaatual)
+    {
+        if($mes == $mesatual)
+        {
+                echo "<script>alert('Este agendamento é impossível pois não podemos voltar ao passado :)');
+                var r = confirm('Você será redirecionado para a sua página principal'); if(r == true){ window.location.href='../paginas/index.php';}</script>";
+                $contador++;
+        }
+    }
+
+    if($dia == $diaatual)
+    {
+        if($mes == $mesatual)
+        {
+            if($hora < $atual){
+                echo "<script>alert('Este agendamento é impossível pois não podemos voltar ao passado :)');
+                var r = confirm('Você será redirecionado para a sua página principal'); if(r == true){ window.location.href='../paginas/index.php';}</script>";
+                $contador++;
+            }
+        }
+    }
+
     if($ano < 2019)
     {
     echo "<script>alert('Este agendamento é impossível pois não podemos voltar ao passado :)');
@@ -65,6 +105,6 @@
 
     }
     else{
-        echo"<script>alert('error')</script>";
+        echo"<script>alert('Esse agendamento foi cancelado :)')</script>";
     }
 ?>  
