d204b69cc12949ce58f5fbb0d567e58494c3b200
Luiz Guilherme <geekanimesamvs@gmail.com>
2019-12-03 16:22:47 -0300

    banco que to usando

--- db_bellprox.sql
+++ db_bellprox.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Máquina: localhost
--- Data de Criação: 26-Nov-2019 às 21:56
+-- Data de Criação: 03-Dez-2019 às 19:21
 -- Versão do servidor: 5.6.13
 -- versão do PHP: 5.4.17
 
@@ -38,231 +38,32 @@ CREATE TABLE IF NOT EXISTS `tb_agendamento` (
   PRIMARY KEY (`cd_agendamento`),
   KEY `fk_tb_agendamento_tb_cliente_idx` (`cd_cliente`),
   KEY `fk_tb_agendamento_tb_profissional_idx` (`cd_profissional`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=21 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=38 ;
 
 --
 -- Extraindo dados da tabela `tb_agendamento`
 --
 
--- MySQL Workbench Forward Engineering
-
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema db_bellprox
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema db_bellprox
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `db_bellprox` DEFAULT CHARACTER SET latin1 ;
-USE `db_bellprox` ;
-
--- -----------------------------------------------------
--- Table `db_bellprox`.`tb_cliente`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_bellprox`.`tb_cliente` (
-  `cd_cliente` INT(11) NOT NULL AUTO_INCREMENT,
-  `nm_cliente` VARCHAR(256) NULL DEFAULT NULL,
-  `cd_tel_fixo_cliente` VARCHAR(250) NULL DEFAULT NULL,
-  `cd_tel_cell_cliente` VARCHAR(250) NULL DEFAULT NULL,
-  `nm_email_cliente` VARCHAR(256) NULL DEFAULT NULL,
-  `cd_cpf_cliente` VARCHAR(250) NULL DEFAULT NULL,
-  `dt_nasc_cliente` VARCHAR(45) NULL DEFAULT NULL,
-  `cd_senha_cliente` VARCHAR(256) NULL DEFAULT NULL,
-  `ds_caminho_img` VARCHAR(256) NULL DEFAULT NULL,
-  PRIMARY KEY (`cd_cliente`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 5
-DEFAULT CHARACTER SET = utf8;
-
-
--- -----------------------------------------------------
--- Table `db_bellprox`.`tb_profissional`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_bellprox`.`tb_profissional` (
-  `cd_profissional` INT(11) NOT NULL AUTO_INCREMENT,
-  `nm_profissional` VARCHAR(256) NULL DEFAULT NULL,
-  `cd_tel_fixo_profissional` VARCHAR(250) NULL DEFAULT NULL,
-  `cd_tel_cell_profissional` VARCHAR(250) NULL DEFAULT NULL,
-  `nm_email_profissional` VARCHAR(256) NULL DEFAULT NULL,
-  `cd_cpf_profissional` VARCHAR(250) NULL DEFAULT NULL,
-  `dt_nasc_profissional` DATE NULL DEFAULT NULL,
-  `cd_senha_profissional` VARCHAR(256) NULL DEFAULT NULL,
-  `ds_profissional` VARCHAR(256) NULL DEFAULT NULL,
-  `ds_caminho_img` VARCHAR(256) NULL DEFAULT NULL,
-  PRIMARY KEY (`cd_profissional`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 4
-DEFAULT CHARACTER SET = utf8;
-
-
--- -----------------------------------------------------
--- Table `db_bellprox`.`tb_agendamento`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_bellprox`.`tb_agendamento` (
-  `cd_agendamento` INT(11) NOT NULL AUTO_INCREMENT,
-  `dt_agendamento` DATE NULL DEFAULT NULL,
-  `hr_agendamento` TIME NULL DEFAULT NULL,
-  `cd_cliente` INT(11) NULL DEFAULT NULL,
-  `cd_profissional` INT(11) NULL DEFAULT NULL,
-  `ds_confirmacao_agendamento` TINYINT(1) NULL DEFAULT NULL,
-  PRIMARY KEY (`cd_agendamento`),
-  INDEX `fk_tb_agendamento_tb_cliente_idx` (`cd_cliente`),
-  INDEX `fk_tb_agendamento_tb_profissional_idx` (`cd_profissional`),
-  CONSTRAINT `fk_tb_agendamento_tb_cliente`
-    FOREIGN KEY (`cd_cliente`)
-    REFERENCES `db_bellprox`.`tb_cliente` (`cd_cliente`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_tb_agendamento_tb_profissional`
-    FOREIGN KEY (`cd_profissional`)
-    REFERENCES `db_bellprox`.`tb_profissional` (`cd_profissional`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-AUTO_INCREMENT = 20
-DEFAULT CHARACTER SET = utf8;
-
-
--- -----------------------------------------------------
--- Table `db_bellprox`.`tb_endereco_cliente_profissional`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_bellprox`.`tb_endereco_cliente_profissional` (
-  `cd_tb_endereco_c_p` INT(11) NOT NULL AUTO_INCREMENT,
-  `cd_profissional` INT(11) NULL DEFAULT NULL,
-  `cd_cliente` INT(11) NULL DEFAULT NULL,
-  PRIMARY KEY (`cd_tb_endereco_c_p`),
-  INDEX `tb_endereco_c_p_cliente_idx` (`cd_cliente`),
-  INDEX `tb_endereco_c_p_profissional_idx` (`cd_profissional`),
-  CONSTRAINT `fk_endereco_c_p_cliente`
-    FOREIGN KEY (`cd_cliente`)
-    REFERENCES `db_bellprox`.`tb_cliente` (`cd_cliente`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_endereco_c_p_profissional`
-    FOREIGN KEY (`cd_profissional`)
-    REFERENCES `db_bellprox`.`tb_profissional` (`cd_profissional`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-AUTO_INCREMENT = 2
-DEFAULT CHARACTER SET = utf8;
-
-
--- -----------------------------------------------------
--- Table `db_bellprox`.`tb_endereco`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_bellprox`.`tb_endereco` (
-  `cd_endereco` INT(11) NOT NULL AUTO_INCREMENT,
-  `cep` VARCHAR(250) NULL DEFAULT NULL,
-  `nm_rua` VARCHAR(250) NULL DEFAULT NULL,
-  `nm_bairro` VARCHAR(250) NULL DEFAULT NULL,
-  `nm_cidade` VARCHAR(250) NULL DEFAULT NULL,
-  `nm_estado` VARCHAR(250) NULL DEFAULT NULL,
-  `cd_tb_endereco_c_p` INT(11) NULL DEFAULT NULL,
-  PRIMARY KEY (`cd_endereco`),
-  INDEX `fk_endereco_tb_endereco_c_p_idx` (`cd_tb_endereco_c_p`),
-  CONSTRAINT `fk_endereco_tb_endereco_c_p`
-    FOREIGN KEY (`cd_tb_endereco_c_p`)
-    REFERENCES `db_bellprox`.`tb_endereco_cliente_profissional` (`cd_tb_endereco_c_p`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-AUTO_INCREMENT = 13
-DEFAULT CHARACTER SET = utf8;
-
-
--- -----------------------------------------------------
--- Table `db_bellprox`.`tb_servico`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_bellprox`.`tb_servico` (
-  `cd_servico` INT(11) NOT NULL AUTO_INCREMENT,
-  `nm_servico` VARCHAR(256) NULL DEFAULT NULL,
-  `ds_servico` VARCHAR(256) NULL DEFAULT NULL,
-  PRIMARY KEY (`cd_servico`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 16
-DEFAULT CHARACTER SET = utf8;
-
-
--- -----------------------------------------------------
--- Table `db_bellprox`.`tb_servico_agendamento`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_bellprox`.`tb_servico_agendamento` (
-  `cd_servico_agendamento` INT(11) NOT NULL AUTO_INCREMENT,
-  `cd_agendamento` INT(11) NULL DEFAULT NULL,
-  `cd_servico` INT(11) NULL DEFAULT NULL,
-  `vl_servico` DECIMAL(10,0) NULL DEFAULT NULL,
-  `cd_avaliacao_profissional` INT(11) NULL DEFAULT NULL,
-  PRIMARY KEY (`cd_servico_agendamento`),
-  INDEX `fk_servico_agendamento_agendamento_idx` (`cd_agendamento`),
-  INDEX `fk_servico_agendamento_servico_idx` (`cd_servico`),
-  CONSTRAINT `fk_servico_agendamento_agendamento`
-    FOREIGN KEY (`cd_agendamento`)
-    REFERENCES `db_bellprox`.`tb_agendamento` (`cd_agendamento`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_servico_agendamento_servico`
-    FOREIGN KEY (`cd_servico`)
-    REFERENCES `db_bellprox`.`tb_servico` (`cd_servico`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-AUTO_INCREMENT = 2
-DEFAULT CHARACTER SET = utf8;
-
-
--- -----------------------------------------------------
--- Table `db_bellprox`.`tb_servico_profissional`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_bellprox`.`tb_servico_profissional` (
-  `cd_servico_profissional` INT(11) NOT NULL AUTO_INCREMENT,
-  `cd_profissional` INT(11) NULL DEFAULT NULL,
-  `cd_servico` INT(11) NULL DEFAULT NULL,
-  PRIMARY KEY (`cd_servico_profissional`),
-  INDEX `fk_servico_profissional_tb_profissional_idx` (`cd_profissional`),
-  INDEX `fk_servico_profissional_tb_servico_idx` (`cd_servico`),
-  CONSTRAINT `fk_servico_profissional_tb_profissional`
-    FOREIGN KEY (`cd_profissional`)
-    REFERENCES `db_bellprox`.`tb_profissional` (`cd_profissional`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_servico_profissional_tb_servico`
-    FOREIGN KEY (`cd_servico`)
-    REFERENCES `db_bellprox`.`tb_servico` (`cd_servico`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
-
--- -----------------------------------------------------
--- Table `db_bellprox`.`tb_portifolio`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_bellprox`.`tb_portifolio` (
-  `cd_portifolio` INT(11) NOT NULL AUTO_INCREMENT,
-  `ds_caminho_img` VARCHAR(256) NULL,
-  `cd_profissional` INT NULL,
-  PRIMARY KEY (`cd_portifolio`),
-  INDEX `fk_protifolio_tb_profissional_idx` (`cd_profissional`),
-  CONSTRAINT `fk_protifolio_tb_profissional`
-    FOREIGN KEY (`cd_profissional`)
-    REFERENCES `db_bellprox`.`tb_profissional` (`cd_profissional`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
--- INSERT 
-
 INSERT INTO `tb_agendamento` (`cd_agendamento`, `dt_agendamento`, `hr_agendamento`, `cd_cliente`, `cd_profissional`, `ds_confirmacao_agendamento`) VALUES
 (7, '2019-11-02', '23:59:00', 2, 3, 1),
-(20, '3232-03-31', '03:23:00', 2, 3, NULL);
+(20, '3232-03-31', '03:23:00', 2, 3, 1),
+(21, '2019-12-26', '20:34:00', 4, 3, NULL),
+(22, '2020-07-27', '13:00:00', 4, 3, NULL),
+(23, '2001-06-01', '23:23:00', 5, 3, 1),
+(24, '2033-12-24', '23:00:00', 5, 3, NULL),
+(25, '2019-12-13', '13:00:00', 5, 3, NULL),
+(26, '2019-12-03', '13:00:00', 5, 3, NULL),
+(27, '2022-12-01', '13:00:00', 5, 3, NULL),
+(28, '2019-12-03', '13:00:00', 5, 3, NULL),
+(29, '2030-12-13', '13:00:00', 5, 3, NULL),
+(30, '2019-01-13', '12:03:00', 5, 3, NULL),
+(31, '2039-12-13', '13:00:00', 5, 3, NULL),
+(32, '2030-12-03', '13:00:00', 5, 3, NULL),
+(33, '2019-12-03', '01:19:00', 5, 3, NULL),
+(34, '2019-12-13', '13:00:00', 5, 3, NULL),
+(35, '2019-12-02', '13:00:00', 5, 3, NULL),
+(36, '2019-12-03', '17:58:00', 5, 3, NULL),
+(37, '2019-01-13', '13:00:00', 5, 3, NULL);
 
 -- --------------------------------------------------------
 
@@ -291,8 +92,30 @@ INSERT INTO `tb_cliente` (`cd_cliente`, `nm_cliente`, `cd_tel_fixo_cliente`, `cd
 (1, 'Yago Silva de Jesus', '1334063601', '2147483647', 'yago@hotmail.com', '1822831008', '1996/02/20', '123', NULL),
 (2, 'davi', '1334638303', '2147483647', 'davi@gmaiil.com', '2147483647', '2002/09/15', '123', ''),
 (3, 'Davi', NULL, '0', 'davi@gmail.com', NULL, NULL, '123', NULL),
-(4, 'davi ', NULL, '(13) 9978-54245', 'davi@hotmail.com', NULL, NULL, '123', NULL),
-(5, 'Paulo', NULL, '139991178578', 'paulo@hotmail.com', NULL, NULL, '123', 'avatar.png');
+(4, 'davi ', NULL, '(13) 9978-54245', 'davi@hotmail.com', NULL, NULL, '123', 'Screenshot_2.png'),
+(5, 'Paulo', NULL, '139991178578', 'paulo@hotmail.com', NULL, NULL, '123', 'Screenshot_1.png');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_endereco`
+--
+
+CREATE TABLE IF NOT EXISTS `tb_endereco` (
+  `cd_endereco` int(11) NOT NULL AUTO_INCREMENT,
+  `cep` varchar(250) DEFAULT NULL,
+  `nm_rua` varchar(250) DEFAULT NULL,
+  `nm_bairro` varchar(250) DEFAULT NULL,
+  `nm_cidade` varchar(250) DEFAULT NULL,
+  `nm_estado` varchar(250) DEFAULT NULL,
+  `cd_tb_endereco_c_p` int(11) DEFAULT NULL,
+  PRIMARY KEY (`cd_endereco`),
+  KEY `fk_endereco_tb_endereco_c_p_idx` (`cd_tb_endereco_c_p`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Extraindo dados da tabela `tb_endereco`
+--
 
 INSERT INTO `tb_endereco` (`cd_endereco`, `cep`, `nm_rua`, `nm_bairro`, `nm_cidade`, `nm_estado`, `cd_tb_endereco_c_p`) VALUES
 (1, '11345045', 'irma maria', 'samarita', 'são vicente', 'São Paulo', 1);
@@ -310,7 +133,7 @@ CREATE TABLE IF NOT EXISTS `tb_endereco_cliente_profissional` (
   PRIMARY KEY (`cd_tb_endereco_c_p`),
   KEY `tb_endereco_c_p_cliente_idx` (`cd_cliente`),
   KEY `tb_endereco_c_p_profissional_idx` (`cd_profissional`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -364,7 +187,7 @@ CREATE TABLE IF NOT EXISTS `tb_servico` (
   `nm_servico` varchar(256) DEFAULT NULL,
   `ds_servico` varchar(256) DEFAULT NULL,
   PRIMARY KEY (`cd_servico`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=17 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=34 ;
 
 --
 -- Extraindo dados da tabela `tb_servico`
@@ -381,7 +204,45 @@ INSERT INTO `tb_servico` (`cd_servico`, `nm_servico`, `ds_servico`) VALUES
 (13, '', 'Barba e cabelo'),
 (14, 'Barba e Cabelo', 'Corte com a familia'),
 (15, 'Corte de cabelo', 'Preciso de corte de cabelo e Maquiagem '),
-(16, '232332', '32323');
+(16, '232332', '32323'),
+(17, 'Lalala', 'Massagem'),
+(18, 'lalala', 'cabelo'),
+(19, '12312', '12311'),
+(20, 'HidrataÃ§Ã£o', 'sabonete nÃ£o sei das quantas'),
+(21, '13', '13'),
+(22, '13', '13'),
+(23, '13', '13'),
+(24, '0', '0'),
+(25, '3', '3'),
+(26, '13', '13'),
+(27, '13', '13'),
+(28, '13', '13'),
+(29, 'agora sim', 'agora sim'),
+(30, 'aee', 'aee'),
+(31, '1300', '1300'),
+(32, '00', '00'),
+(33, '13', '13');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_servico_agendamento`
+--
+
+CREATE TABLE IF NOT EXISTS `tb_servico_agendamento` (
+  `cd_servico_agendamento` int(11) NOT NULL AUTO_INCREMENT,
+  `cd_agendamento` int(11) DEFAULT NULL,
+  `cd_servico` int(11) DEFAULT NULL,
+  `vl_servico` decimal(10,0) DEFAULT NULL,
+  `cd_avaliacao_profissional` int(11) DEFAULT NULL,
+  PRIMARY KEY (`cd_servico_agendamento`),
+  KEY `fk_servico_agendamento_agendamento_idx` (`cd_agendamento`),
+  KEY `fk_servico_agendamento_servico_idx` (`cd_servico`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Extraindo dados da tabela `tb_servico_agendamento`
+--
 
 INSERT INTO `tb_servico_agendamento` (`cd_servico_agendamento`, `cd_agendamento`, `cd_servico`, `vl_servico`, `cd_avaliacao_profissional`) VALUES
 (1, 7, 0, '424', 1);
@@ -401,10 +262,43 @@ CREATE TABLE IF NOT EXISTS `tb_servico_profissional` (
   KEY `fk_servico_profissional_tb_servico_idx` (`cd_servico`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `tb_endereco`
+--
+ALTER TABLE `tb_endereco`
+  ADD CONSTRAINT `fk_endereco_tb_endereco_c_p` FOREIGN KEY (`cd_tb_endereco_c_p`) REFERENCES `tb_endereco_cliente_profissional` (`cd_tb_endereco_c_p`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Limitadores para a tabela `tb_endereco_cliente_profissional`
+--
+ALTER TABLE `tb_endereco_cliente_profissional`
+  ADD CONSTRAINT `fk_endereco_c_p_cliente` FOREIGN KEY (`cd_cliente`) REFERENCES `tb_cliente` (`cd_cliente`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_endereco_c_p_profissional` FOREIGN KEY (`cd_profissional`) REFERENCES `tb_profissional` (`cd_profissional`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Limitadores para a tabela `tb_portifolio`
+--
+ALTER TABLE `tb_portifolio`
+  ADD CONSTRAINT `fk_protifolio_tb_profissional` FOREIGN KEY (`cd_profissional`) REFERENCES `tb_profissional` (`cd_profissional`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Limitadores para a tabela `tb_servico_agendamento`
+--
+ALTER TABLE `tb_servico_agendamento`
+  ADD CONSTRAINT `fk_servico_agendamento_agendamento` FOREIGN KEY (`cd_agendamento`) REFERENCES `tb_agendamento` (`cd_agendamento`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_servico_agendamento_servico` FOREIGN KEY (`cd_servico`) REFERENCES `tb_servico` (`cd_servico`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Limitadores para a tabela `tb_servico_profissional`
+--
+ALTER TABLE `tb_servico_profissional`
+  ADD CONSTRAINT `fk_servico_profissional_tb_profissional` FOREIGN KEY (`cd_profissional`) REFERENCES `tb_profissional` (`cd_profissional`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_servico_profissional_tb_servico` FOREIGN KEY (`cd_servico`) REFERENCES `tb_servico` (`cd_servico`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
